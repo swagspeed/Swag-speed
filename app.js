377542caddb51a37401a6604420562c3b7003c5a
swagspeed <mkdevilgaming@gmail.com>
2026-06-26 22:22:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
+function customer() {
+    alert("Welcome Customer!");
+}
 
+function seller() {
+    alert("Welcome Seller!");
+}
+
+function delivery() {
+    alert("Welcome Delivery Partner!");
+}
+
+function admin() {
+    alert("Welcome Admin!");
+}
